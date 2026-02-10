cf14807d9dccdd42c2a9d896b7b6e1abcc3dbd36
NicolasTremblayENS <ntremblay@cegepthetford.ca>
2026-02-10 09:50:27 -0500

    Mise à jours

--- Script/Arduino_to_Python_Exemple/Arduino_to_Python_Exemple.ino
+++ Script/Arduino_to_Python_Exemple/Arduino_to_Python_Exemple.ino
@@ -1,29 +1,38 @@
 const int redLight = 2;
 const int intLight = 13;
+const int button = 6;
 
 void setup() {
   Serial.begin(9600);
   pinMode(redLight, OUTPUT);
   pinMode(intLight, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
-  if (Serial.available() > 0){        //  Regarde s'il y a un message dans le serial
-    String msg = Serial.readString(); //  Lit le message
-
-    if (msg == "ON"){
-      digitalWrite(intLight, HIGH);
-    }
-    else if (msg == "OFF"){
-      digitalWrite(intLight, LOW);
-    }
-    else {
-      for (int i = 0; i < 5; i++){
-        digitalWrite(redLight, HIGH);
-        delay(100);
-        digitalWrite(redLight, LOW);
-        delay(100);
-      }
-    }
+  int start = digitalRead(button);
+  if (start > 0){
+    digitalWrite(intLight, HIGH)
   }
+  else {
+    digitalWrite(intLight, LOW)
+  }
+  // if (Serial.available() > 0){        //  Regarde s'il y a un message dans le serial
+  //   String msg = Serial.readString(); //  Lit le message
+
+  //   if (msg == "ON"){
+  //     digitalWrite(intLight, HIGH);
+  //   }
+  //   else if (msg == "OFF"){
+  //     digitalWrite(intLight, LOW);
+  //   }
+  //   else {
+  //     for (int i = 0; i < 5; i++){
+  //       digitalWrite(redLight, HIGH);
+  //       delay(100);
+  //       digitalWrite(redLight, LOW);
+  //       delay(100);
+  //     }
+  //   }
+  // }
 }
