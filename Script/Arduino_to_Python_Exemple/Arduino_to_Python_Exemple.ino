ed6711dd80576dcf3f09dcee3cebfcc9d23863f0
NicolasTremblayENS <ntremblay@cegepthetford.ca>
2026-02-10 11:34:29 -0500

    Version Fonctionnelle, ON/OFF/BOUTON

--- Script/Arduino_to_Python_Exemple/Arduino_to_Python_Exemple.ino
+++ Script/Arduino_to_Python_Exemple/Arduino_to_Python_Exemple.ino
@@ -1,39 +1,39 @@
 const int redLight = 2;
 const int intLight = 13;
 const int button = 6;
+int cnt = 0;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(redLight, OUTPUT);
   pinMode(intLight, OUTPUT);
   pinMode(button, INPUT_PULLUP);
 }
 
 void loop() {
-  int start = digitalRead(button);
-  Serial.println(start);
-  if (start < 1){
-    digitalWrite(intLight, HIGH);
+  if (Serial.available() > 0){        //  Regarde s'il y a un message dans le serial
+    String msg = Serial.readString(); //  Lit le message
+    if (msg == "ON"){
+      digitalWrite(intLight, HIGH);
+    }
+    else if (msg == "OFF"){
+      digitalWrite(intLight, LOW);
+    }
+    else {
+      for (int i = 0; i < 5; i++){
+        digitalWrite(redLight, HIGH);
+        delay(100);
+        digitalWrite(redLight, LOW);
+        delay(100);
+      }
+    }
   }
-  else {
-    digitalWrite(intLight, LOW);
+  int toggle = digitalRead(button);
+  if (toggle < 1){
+    Serial.println("toggle");
   }
-  // if (Serial.available() > 0){        //  Regarde s'il y a un message dans le serial
-  //   String msg = Serial.readString(); //  Lit le message
-
-  //   if (msg == "ON"){
-  //     digitalWrite(intLight, HIGH);
-  //   }
-  //   else if (msg == "OFF"){
-  //     digitalWrite(intLight, LOW);
-  //   }
-  //   else {
-  //     for (int i = 0; i < 5; i++){
-  //       digitalWrite(redLight, HIGH);
-  //       delay(100);
-  //       digitalWrite(redLight, LOW);
-  //       delay(100);
-  //     }
-  //   }
-  // }
+  else{
+    Serial.println("not toggle");
+  }
+  delay(100);
 }
--- Script/Arduino_to_Python_Exemple/Arduino_to_Python_Exemple.py
+++ Script/Arduino_to_Python_Exemple/Arduino_to_Python_Exemple.py
@@ -1,7 +1,8 @@
 import serial.tools.list_ports
+import time as t
 
 serialInst = serial.Serial()
-serialInst.baudrate = 9600
+serialInst.baudrate = 115200
 ports = serial.tools.list_ports.comports()
 portsList = []
 
@@ -9,26 +10,37 @@ for p in ports:
     portsList.append(str(p))
     print(str(p))
 
-if portsList:
-    com = input("Select Arduino port by #: ")
-else:
+if len(portsList) == 0:
     com = ""
     print("Connect Arduino to Computer")
-
-
-for i in range(len(portsList)):
-    if portsList[i].startswith("COM" + com):
-        com = "COM" + str(com)
-        print(com + " is selected")
-    else:
-        print("Select valid COM number")
+elif len(portsList) == 1:
+    com = portsList[0]
+    com = com[:4]
+else:
+    com = input("Select Arduino port by #: ")
+    for i in range(len(portsList)):
+        if portsList[i].startswith("COM" + com):
+            com = "COM" + str(com)
+            print(com + " is selected")
+        else:
+            print("Select valid COM number")
 
 serialInst.port = com
+t.sleep(1)
+serialInst.close()
 serialInst.open()
+print(com + " is connected")
 
 while True:
-    command = input("Arduino Command (ON, OFF, exit): ")
-    serialInst.write(command.encode("utf-8"))
-
-    if command == "exit":
+    command = input("Arduino Command (ON, OFF, STATE, exit): ")
+    if command == "ON" or command == "OFF":
+        serialInst.write(command.encode("utf-8"))
+    elif command == "STATE":
+        for i in range(10):
+            if serialInst.inWaiting() == 0:
+                pass
+            serialInst.reset_input_buffer()
+            print(str(serialInst.readline().decode("utf-8").strip()))
+            t.sleep(0.1)
+    elif command == "exit":
         exit()
